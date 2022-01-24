d1629f6c4643b7c2318a30f790801df7d2734936
Дарья Дубровская <>
2022-01-25 02:24:14 +0300

    Task 11 - Navigation: Task completed

---
+++ HomeWork11Navigation/HomeWork11Navigation/ViewContollers/SecondFileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondFileViewController.swift
+//  HomeWork11Navigation
+//
+//  Created by Дарья Дубровская on 25.01.22.
+//
+
+import UIKit
+
+class SecondFileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
